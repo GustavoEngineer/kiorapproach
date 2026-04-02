85830d0004e888cc280543724e3844a1183e83a2
Gustavo Marrufo <gustavitotlgac@gmail.com>
2026-04-01 18:35:17 -0600

    Configuración de base de datos

--- backend/api/mongo/api/index.js
+++ backend/api/mongo/api/index.js
@@ -1,4 +1,13 @@
+// c:\Room\ItsMe\KiorApproach\backend\api\mongo\api\index.js
 const app = require('../src/app');
+const connectDB = require('../src/config/database');
 
-// Exportamos la app para que Vercel la use como función serverless
-module.exports = app;
+let isConnected = false;
+
+module.exports = async (req, res) => {
+    if (!isConnected) {
+        await connectDB();
+        isConnected = true;
+    }
+    return app(req, res);
+};
